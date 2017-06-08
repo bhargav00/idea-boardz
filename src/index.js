c6f5aa05eccfb31682d5289f547bafe0ec0cb8e4
bhargav00 <bmaniar00@gmail.com>
2017-06-08 12:03:14 +0530

    Index files added

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import MainComponent from './components/main.js';
+
+render(
+    <MainComponent/>,
+    document.getElementById('app')
+);
