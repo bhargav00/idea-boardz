86331e103511fbf3cc9956918ebf41a87cc60fd7
bhargav00 <bmaniar00@gmail.com>
2017-06-08 16:41:48 +0530

    Teo diffrent views

--- src/components/note.js
+++ src/components/note.js
@@ -1,23 +1,43 @@
 import React from 'react';
 
 export default class Note extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            editing: true
+        };
+    }
     render() {
-        return (
-            <div className="col s12 m6">
-                <div className="noteContainer card blue-grey darken-1">
-                    <div className="noteText card-content white-text">
-                        <div class="input-field col s6">
-                            <label for="textarea1">Note</label>
-                            <textarea id="textarea1" className="materialize-textarea" ref="newText"></textarea>
+        return (this.state.editing == true
+            ? <div className="col s12 m6">
+                    <div className="noteContainer card blue-grey darken-1">
+                        <div className="noteText card-content white-text">
+                            <div className="input-field col s6">
+                                <label htmlFor="textarea1">Note</label>
+                                <textarea
+                                    id="textarea1"
+                                    className="materialize-textarea"
+                                    ref="newText"
+                                    defaultValue={this.props.children}></textarea>
+                            </div>
+                        </div>
+                        <div className="card-action">
+                            <button className="waves-effect waves-light btn">Save</button>
+                            <button className="waves-effect waves-light btn red darken-4">Remove</button>
                         </div>
                     </div>
+                </div>
+            : <div className="col s12 m6">
+                <div className="noteContainer card blue-grey darken-1">
+                    <div className="noteText card-content white-text">
+                        <span className="card-title">{this.props.children}</span>
+                    </div>
                     <div className="card-action">
-                        <button className="waves-effect waves-light btn">Save</button>
+                        <button onClick={()=>{this.edit}} className="waves-effect waves-light btn">Edit</button>
                         <button className="waves-effect waves-light btn red darken-4">Remove</button>
                     </div>
                 </div>
-            </div>
-        )
+            </div>)
     }
 
 }
