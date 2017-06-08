b32e24b988e5533f7ae5e1c62b08dd2aeb69ee97
bhargav00 <bmaniar00@gmail.com>
2017-06-08 12:10:24 +0530

    main component

---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class MainComponent extends React.Component {
+        render() {
+            return ( 
+                <div className="row">
+                <div className="col s12 add-note-container">
+                  
+                </div>
+              </div>)
+            }
+
+
+        }
