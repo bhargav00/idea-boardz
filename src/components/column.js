ca702099ddb975a752f6ebe4a01e872896d0d74f
bhargav00 <bmaniar00@gmail.com>
2017-06-08 15:44:26 +0530

    Add node logic added

--- src/components/column.js
+++ src/components/column.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 export default class Column extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: []
+        };
+    }
+    add(text) {
+        // store notes array into variable
+        var arr = this.state.notes;
+        // push new note into array
+        arr.push(text);
+        // update array
+        this.setState({notes: arr});
+    }
     render() {
         return (
-                <div className="col s4 add-note-container">
-                    <button className="waves-effect waves-light btn-large new-note-btn">Add New Note</button>
-                </div>
+            <div className="col s4 add-note-container">
+                <button onClick={()=>{this.add("Add a new note!")}} className="waves-effect waves-light btn-large new-note-btn">Add New Note</button>
+            </div>
         )
     }
 
