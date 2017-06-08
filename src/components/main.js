35e4e8621c94b99b088b3439f6d505488560a5fe
bhargav00 <bmaniar00@gmail.com>
2017-06-08 20:21:12 +0530

    Titles added

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Column from './column';
+import '../css/main.css'
 export default class MainComponent extends React.Component {
     render() {
         return (
             <div className="row">
-               <Column/>
-               <Column/>
-               <Column/>
+               <Column title="Things that went well"/>
+               <Column title="Improvements"/>
+               <Column title="Action items"/>
             </div>
         )
     }
